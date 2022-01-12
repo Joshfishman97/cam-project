ef1a8ef47bb8be5f6db5239ba5701ce45b1e22f2
Josh Fishman <Fishmanjosh251@gmail.com>
2022-01-12 15:59:37 -0600

    created navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            <h1>SpaceisDangerous</h1>
+            <div>
+                <a href="/">Home</a>
+                <a href="/content">Content</a>
+            </div>
+        </nav>
+     );
+}
+ 
+export default Navbar;
